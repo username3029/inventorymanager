4e2745e0fadf28968280b3901a050566b82404e0
username3029 <tomar.dev@hotmail.com>
2025-04-12 19:04:21 +0530

    Add utils.h with clear_console and input validation functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "splashkit.h"
+#include "models.h"  // Include data model definitions
+#include "utils.h"   // Include utility functions
 
 int main() {
     write_line("Welcome to the Inventory Manager!");
